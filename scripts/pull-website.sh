40660381d4b822a30d20175cce48341bd20a7d6c
erwan-blancard <erwan.blancard@laplateforme.io>
2022-12-23 10:13:38 +0100

    ajout du script pour pull le site

--- scripts/pull-website.sh
+++ scripts/pull-website.sh
@@ -7,3 +7,6 @@ cd /chemin/vers/le/repo
 git fetch --all
 git checkout origin/main -- website/index.html
 git checkout origin/main -- website/style.css
+
+mv website/index.html /var/www/html/index.html
+mv website/style.css /var/www/html/style.css
